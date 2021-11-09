f651a341583963f4a073656a57ef485a918fd7d3
Michael Van Brummen <michaelvanbrummen@gmail.com>
2021-11-09 19:35:26 +1000

    Added README and docker compose

---
+++ README.md
@@ -0,0 +1,19 @@
+# Kafka Tool Swing
+
+Kafka management tool in Java Swing. Never finished it because Swing is so horrible...
+
+![screenshot.png](images/screenshot.png "Screen shot")
+
+# Getting started 
+
+Start a local kafka
+```
+docker-compose up -d
+```
+
+Build and run the app
+
+
+
+
+
--- src/main/java/mvanbrummen/kafka/App.java
+++ src/main/java/mvanbrummen/kafka/App.java
@@ -7,19 +7,17 @@ import mvanbrummen.kafka.view.MainView;
 
 import javax.swing.plaf.FontUIResource;
 import java.awt.*;
-import java.io.IOException;
 
 import static mvanbrummen.kafka.utills.FontUtils.setFont;
 
 public class App {
 
-    public static void main(String[] args) throws IOException, FontFormatException {
+    public static void main(String[] args) {
         // on macOS enable screen menu bar
         if (SystemInfo.isMacOS && System.getProperty("apple.laf.useScreenMenuBar") == null)
             System.setProperty("apple.laf.useScreenMenuBar", "true");
 
         FlatLightLaf.setup();
-//        FlatLightLaf.setup(new FlatDarkLaf());
 
         setFont(new FontUIResource(new Font("Cabin", Font.PLAIN, 14)));
 
